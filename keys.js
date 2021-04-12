7c397959323011e47c070ac912245617ba4d9a64
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-04-12 18:49:53 +0530

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,7 @@
+// API KEY => https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = 'AIzaSyCbpSenQzI_Syx1npSV8TclL9EKq9KLSH0'
+
+
+// context key => the link is used to create a context key https://cse.google.com/cse/create/new
+
+export const CONTEXT_KEY = 'ff594588ed5e391c6'
